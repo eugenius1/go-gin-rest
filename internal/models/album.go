6029a0beda19e65b8377b7416910886b59cd36ea
Eusebius Ngemera <eugenius1@users.noreply.github.com>
2024-09-10 00:45:43 +0000

    feat: Remove timestamps from album JSON
    To pass controller test

--- internal/models/album.go
+++ internal/models/album.go
@@ -1,13 +1,18 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 // Album represents data about a record Album.
 type Album struct {
-	gorm.Model
-	ID     string  `json:"id"` // this json tag overrides the default one in gorm.Model
+	ID        string         `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
